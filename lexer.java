b99badf94f8b391233e1256158344e1a88a34f80
Nicholas <45800061+aceofdiamonds4500@users.noreply.github.com>
2026-05-03 22:30:57 -0400

    Update lexer.java

--- lexer.java
+++ lexer.java
@@ -1,15 +1,21 @@
-// mind you, this has a lot of example code
 lexer grammar ExprLexer;
 
 ADD: '+';
 SUB: '-';
 MUL: '*';
 DIV: '/';
-AND : 'and' ;
-OR : 'or' ;
-NOT : 'not' ;
+AND : '&&' ;
+OR : '|' ;
+NOT : '!' ;
 BECAUSE: 'because';
 EQ : '=' ;
+
+EQUIV: '==' ;
+LT: '<';
+LTE: '<=';
+GT: '>';
+GTE: '>=';
+
 COMMA : ',' ;
 SEMI : ';' ;
 LPAREN : '(' ;
@@ -17,7 +23,24 @@ RPAREN : ')' ;
 LCURLY : '{' ;
 RCURLY : '}' ;
 NEEDS: '>>' ;
+EXPLAIN: 'exp';
+//runs libraries and executes conditionals
+RUN: 'run';
+//this allows for explanation bypassing, very weird and mysterious
+I_SAID_SO: 'i_said_so';
+TRUE: 'True';
+FALSE: 'False';
+REPEAT: 'repeat';
+UNTIL: 'until';
+IF: 'if';
+ELSE: 'else';
+
+LINE_COMMENT : '~~' ~[\r\n]* -> channel(HIDDEN) ;
+BLOCK_COMMENT : '~*' .*? '*~' -> channel(HIDDEN) ;
 
 INT : [0-9]+ ;
+STRING : '"' ( ESC | ~('\\' | '"') )* '"' ;
+fragment ESC : '\\' ( [btnfr"\\] ) ;
+
 ID: [a-zA-Z_][a-zA-Z_0-9]* ;
 WS: [ \t\n\r\f]+ -> skip ;
